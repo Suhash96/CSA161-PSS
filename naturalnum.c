ddedb228970c65d6a8c21e38a3218029321779a2
Suhash96 <112850474+Suhash96@users.noreply.github.com>
2023-05-12 08:48:01 +0530

    Add files via upload

---
+++ naturalnum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+	int n,i;
+	printf("natural numbers upto n with a difference of 2");
+	Scanf("%d",&n);
+	for(i=1;i<=n;i=i+2)
+	{
+		printf("%d",i);
+		return 0;
+		
+	}
+	}
+
